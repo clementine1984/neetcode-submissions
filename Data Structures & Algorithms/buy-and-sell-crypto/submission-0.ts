9638689adebafa6175e5bf0079d927a939f163fc
clementine1984 <73428229+clementine1984@users.noreply.github.com>
2026-06-22 18:50:22 +0100

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.ts
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices: number[]): number {
+
+        let maxProfit = 0;
+        let minBuy = prices[0];
+
+        for(let sell of prices){
+           maxProfit = Math.max(maxProfit, sell - minBuy);
+           minBuy = Math.min(minBuy, sell);
+        }
+
+        return maxProfit;
+    }
+}
