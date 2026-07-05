38050db871cfd01966e0126174a40eae3594bac7
clementine1984 <73428229+clementine1984@users.noreply.github.com>
2026-07-05 13:01:14 +0100

    Add: count-paths - submission-0

---
+++ Data Structures & Algorithms/count-paths/submission-0.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int UniquePaths(int m, int n) {
+        
+        int[] dp = new int[n];
+        Array.Fill(dp, 1);
+
+        for(int i = m - 2; i >= 0; i--){
+            for(int j = n - 2; j >= 0; j--){
+                dp[j] += dp[j + 1];
+            }
+        }
+
+        return dp[0];
+    }
+}
