9c1f20ac2cc4a88f45c48cd9ce6aa9352394adfc
clementine1984 <73428229+clementine1984@users.noreply.github.com>
2026-07-05 17:35:44 +0100

    Add: insert-new-interval - submission-0

---
+++ Data Structures & Algorithms/insert-new-interval/submission-0.cs
@@ -0,0 +1,32 @@
+public class Solution {
+    public int[][] Insert(int[][] intervals, int[] newInterval) {
+        
+        //Linear search
+
+        var res = new List<int[]>();
+
+        for(int i = 0; i < intervals.Length; i++){
+           //There are only ever three possible relationships 
+           //between newInterval and the current intervals[i].
+           if(newInterval[1] < intervals[i][0]) {
+            res.Add(newInterval);
+            res.AddRange(intervals.AsEnumerable().Skip(i).ToArray());
+
+            return res.ToArray();
+           }
+
+           else if(newInterval[0] > intervals[i][1]) {
+             res.Add(intervals[i]);
+
+           } else {
+            //They must overlap. Merge them by expanding newInterval to cover both, 
+            //and keep checking,the merged interval might also overlap the next one(s).
+             newInterval[0] = Math.Min(intervals[i][0], newInterval[0]);
+             newInterval[1] = Math.Max(intervals[i][1], newInterval[1]);
+           }
+        }
+
+        res.Add(newInterval);
+        return res.ToArray();
+    }
+}
