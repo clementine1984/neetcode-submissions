90a393d47e5ff3e646714ffc80422586d03f1575
clementine1984 <73428229+clementine1984@users.noreply.github.com>
2026-07-06 16:17:29 +0100

    Add: three-integer-sum - submission-1

---
+++ Data Structures & Algorithms/three-integer-sum/submission-1.cs
@@ -0,0 +1,34 @@
+public class Solution {
+    public List<List<int>> ThreeSum(int[] nums) {
+        
+        //Sort the array first.
+        Array.Sort(nums);
+        List<List<int>> res = new List<List<int>>();
+
+        for(int i = 0; i < nums.Length; i++){
+            if(nums[i] > 0) break; 
+            //Prevent duplicate triplets by skipping one loop.
+            if(i > 0 && nums[i] == nums[i-1]) continue; 
+
+            //Set left and right pointers.
+            int l = i + 1, r = nums.Length - 1;
+            while(l < r){
+                int sum = nums[i] + nums[l] + nums[r];
+                if(sum > 0){
+                    r--;
+                } else if (sum < 0){
+                    l++;
+                } else{
+                    res. Add(new List<int> {nums[i], nums[l], nums[r]});
+                    l++;
+                    r--;
+                    //Avoid duplicate left pointer value
+                    while (l < r && nums[l] == nums[l - 1]) {
+                    l++; 
+                }
+                } 
+            }
+        }
+        return res;
+    }
+}
