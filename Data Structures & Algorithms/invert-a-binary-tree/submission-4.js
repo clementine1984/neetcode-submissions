74cbb2923b031ddfa349952e60a527e33cd6230a
clementine1984 <73428229+clementine1984@users.noreply.github.com>
2026-07-13 20:55:13 +0100

    Add: invert-a-binary-tree - submission-4

---
+++ Data Structures & Algorithms/invert-a-binary-tree/submission-4.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+//DFS, Recursion
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {TreeNode}
+     */
+    invertTree(root) {
+         
+        if (!root) return null;
+
+        //Destructuring Assignment
+        [root.left, root.right] = [this.invertTree(root.right), this.invertTree(root.left)];
+
+        return root;
+        
+    }
+}
