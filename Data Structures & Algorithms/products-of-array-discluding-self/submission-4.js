c543dd1ec88451a4150b5d880fa88465b0e086ba
clementine1984 <73428229+clementine1984@users.noreply.github.com>
2026-07-22 22:31:27 +0100

    Add: products-of-array-discluding-self - submission-4

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-4.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+
+    //Prefix and suffix optimal
+
+    productExceptSelf(nums) {
+
+        const n = nums.length;
+
+        const res = new Array(n);
+        
+        let prefix = 1;  
+        for (let i = 0; i < n; i++) {
+            res[i] = prefix;
+            prefix *= nums[i];
+        }
+
+        let suffix = 1;
+        for (let j = n - 1; j >= 0; j--){
+            res[j] *= suffix;
+            suffix *= nums[j];
+        }
+
+        return res;
+    }
+}
