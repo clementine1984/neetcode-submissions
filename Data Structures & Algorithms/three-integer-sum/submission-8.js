ec2ccb597b010a2cf8d2cc7d798d90c7a9d23165
clementine1984 <73428229+clementine1984@users.noreply.github.com>
2026-07-24 21:41:39 +0100

    Add: three-integer-sum - submission-8

---
+++ Data Structures & Algorithms/three-integer-sum/submission-8.js
@@ -0,0 +1,43 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums) {
+
+        nums.sort((a, b) => a - b);
+
+        const res = [];
+
+        if (nums[0] > 0) return [];
+
+        for (let i = 0; i < nums.length - 2; i++){
+
+            let j = i + 1;
+            let k = nums.length - 1;
+
+            if (nums[i - 1] == nums[i]) continue;
+
+            while (j < k) {
+                let sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0) {
+                  res.push([nums[i],nums[j],nums[k]]);
+                  j++;
+                  k--;
+                  //if (nums[j] === nums[j - 1]) j++;
+                  //while keeps re-checking after every execution.
+                  //The outer while (j < k) loop has no opportunity 
+                  //to catch a problem mid-body, 
+                  //j and k could have changed at this point.
+                  while (j < k && nums[j] === nums[j - 1]) j++;
+                } else if (sum < 0){
+                  j++;
+                } else {
+                  k--;
+                }           
+            }
+            
+        }
+        return res;
+    }
+}
