fda1d1aeec21a828575395ba7f154ef77abaa6ba
clementine1984 <73428229+clementine1984@users.noreply.github.com>
2026-07-27 12:12:07 +0100

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+
+        if (s.length !== t.length) return false;
+        
+        //text.split("")     // Split on characters
+        //text.split(","(    // Split on commas
+        //text.split(" ")    // Split on spaces
+        //text.split("|")    // Split on pipe
+        const sSort = s.split('').sort().join();
+        const tSort = t.split('').sort().join();
+
+        //Nested loop won't work as it alwasy compare 
+        //the first character in sSort 
+        //against every other character in tSort.
+
+        //for (let ss of sSort) {
+        //    for (let ts of tSort) {
+        //        if (ss != ts) {
+        //            return false;
+        //        }
+        //    }
+        //}
+
+        return sSort == tSort;
+    }
+}
