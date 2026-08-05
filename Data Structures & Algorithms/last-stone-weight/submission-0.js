3b98fa3e42653154ee0739c4bfe8768a2e1420a5
clementine1984 <73428229+clementine1984@users.noreply.github.com>
2026-08-05 22:21:51 +0100

    Add: last-stone-weight - submission-0

---
+++ Data Structures & Algorithms/last-stone-weight/submission-0.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} stones
+     * @return {number}
+     */
+    lastStoneWeight(stones) {
+
+        this.maxHeap = new MaxPriorityQueue();
+        
+        for (const stone of stones) {
+            this.maxHeap.enqueue(stone);
+        }
+
+        while (this.maxHeap.size() > 1) {
+            const x = this.maxHeap.front();
+            this.maxHeap.dequeue();
+            const y =  this.maxHeap.front();
+            this.maxHeap.dequeue();
+            this.maxHeap.enqueue(x - y);
+        }
+
+        return this.maxHeap.front();
+    }
+}
