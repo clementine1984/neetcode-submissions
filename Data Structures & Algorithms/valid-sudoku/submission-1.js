8c89f5d8c93928398714dd59c085e1a61ab95627
clementine1984 <73428229+clementine1984@users.noreply.github.com>
2026-08-12 16:56:25 +0100

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.js
@@ -0,0 +1,50 @@
+class Solution {
+    /**
+     * @param {character[][]} board
+     * @return {boolean}
+     */
+    isValidSudoku(board) {
+        // rows
+        for (let i = 0; i < board.length; i++) {
+            let set = new Set();
+            for (let j = 0; j < board[i].length; j++) {
+                const val = board[i][j];
+                //skipping empty cells.
+                if (val === ".") continue;
+                if (set.has(val)) return false;
+                set.add(val);
+            }
+        }
+
+        // columns
+        for (let j = 0; j < board[0].length; j++) {
+            let set = new Set();
+            for (let i = 0; i < board.length; i++) {
+                const val = board[i][j];
+                //skipping empty cells.
+                if (val === ".") continue;
+                if (set.has(val)) return false;
+                set.add(val);
+            }
+        }
+
+        // 3x3 boxes
+        const start = [0, 3, 6];
+        for (let r of start) {
+            for (let c of start) {
+                let set = new Set();
+                for (let i = 0; i < 3; i++) {
+                    for (let j = 0; j < 3; j++) {
+                        const val = board[r + i][c + j];
+                        //skipping empty cells.
+                        if (val === ".") continue;
+                        if (set.has(val)) return false;
+                        set.add(val);
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+}
