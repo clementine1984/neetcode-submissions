d32af9ed1a7ea49547e33cf800c5dc052951532e
clementine1984 <73428229+clementine1984@users.noreply.github.com>
2026-08-16 22:09:06 +0100

    Add: find-duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-0.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    findDuplicate(nums) {
+
+        const set = new Set();
+
+        for (const num of nums) {
+            if (set.has(num)) {
+                return num;
+            } else {
+                set.add(num);
+            }
+        }
+    }
+}
